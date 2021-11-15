b65800fa7a917026419bef7b3ac3b9f87cf4a5e0
Joe Williams <WilliamsJ155@cardiff.ac.uk>
2021-11-15 11:51:24 +0000

    Create Spring project

---
+++ src/main/java/uk/ac/cardiff/team5/graphium/GraphiumApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.cardiff.team5.graphium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphiumApplication.class, args);
+    }
+
+}
