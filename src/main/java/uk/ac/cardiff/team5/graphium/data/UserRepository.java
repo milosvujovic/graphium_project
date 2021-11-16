fb0038ea85bd17e96b76c6a8a7571665b4974049
Joseph Williams <williamsj155@cardiff.ac.uk>
2021-11-16 14:00:07 +0000

    create User jpa entity and change db url

--- src/main/java/uk/ac/cardiff/team5/graphium/data/UserRepository.java
+++
@@ -1,4 +0,0 @@
-package uk.ac.cardiff.team5.graphium.data;
-
-public class UserRepository {
-}
---
+++ src/main/java/uk/ac/cardiff/team5/graphium/data/jpa/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package uk.ac.cardiff.team5.graphium.data.jpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
