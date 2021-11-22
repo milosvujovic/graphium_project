28d924722ee2cec30e1c0e159592c53cf3f5044a
Mohammed Hussain <HussainMS2@cardiff.ac.uk>
2021-11-22 20:19:39 +0000

    Create initial spring security class requirements

---
+++ src/main/java/uk/ac/cardiff/team5/graphium/service/impl/UserDetailsService.java
@@ -0,0 +1,8 @@
+package uk.ac.cardiff.team5.graphium.service.impl;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.ac.cardiff.team5.graphium.data.jpa.repository.UserRepository;
+
+public class UserDetailsService {
+}
