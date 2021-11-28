19e4d44c3b481b2ec0bca0d7beba0e9a09435d14
Joseph Williams <williamsj155@cardiff.ac.uk>
2021-11-28 22:19:12 +0000

    a

---
+++ src/main/java/uk/ac/cardiff/team5/graphium/api/controllers/organisation/OrganisationAPIController.java
@@ -0,0 +1,7 @@
+package uk.ac.cardiff.team5.graphium.api.controllers.organisation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("")
+public class OrganisationAPIController {
+}
---
+++ src/main/java/uk/ac/cardiff/team5/graphium/web/controllers/sysAdminPanel/SysAdminPanelController.java
@@ -0,0 +1,14 @@
+package uk.ac.cardiff.team5.graphium.web.controllers.sysAdminPanel;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SysAdminPanelController {
+    @GetMapping("/sys-admin-panel")
+    public String sysAdminPanel(Model model) {
+        return "sys-admin-panel";
+    }
+}
