311b0e4a93ace52f0c99e479adf9267386e7f29e
Joseph Williams <williamsj155@cardiff.ac.uk>
2021-11-30 17:18:15 +0000

    temp

--- src/main/java/uk/ac/cardiff/team5/graphium/api/controllers/user/UserAPIController.java
+++ src/main/java/uk/ac/cardiff/team5/graphium/api/controllers/user/UserAPIController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.ac.cardiff.team5.graphium.data.jpa.entity.UserEntity;
 import uk.ac.cardiff.team5.graphium.data.jpa.repository.UserRepository;
 import uk.ac.cardiff.team5.graphium.domain.User;
+import uk.ac.cardiff.team5.graphium.service.dto.UserDTO;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController()
 @RequestMapping("api/user")
@@ -19,7 +21,15 @@ public class UserAPIController {
     }
 
     @GetMapping()
-    String all() {
-        return repository.findAll().get(0).toString();
+    List<UserDTO> all() {
+        return repository.findAll().stream()
+                .map(userEntity -> new UserDTO(
+                        userEntity.getFirstName(),
+                        userEntity.getLastName(),
+                        (userEntity.getOrganisation() != null) ? userEntity.getOrganisation().getOrganisationId() : -1,
+                        userEntity.getUsername(),
+                        userEntity.getEmail(),
+                        userEntity.getPassword()
+                )).collect(Collectors.toList());
     }
 }
--- src/main/java/uk/ac/cardiff/team5/graphium/data/jpa/projection/user/UserView.java
+++ src/main/java/uk/ac/cardiff/team5/graphium/data/jpa/projection/user/UserView.java
@@ -1,5 +1,4 @@
 package uk.ac.cardiff.team5.graphium.data.jpa.projection.user;
 
 public interface UserView {
-    String get
 }
