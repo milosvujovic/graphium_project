9a54970dd1dd498d1643de7138d331509080706c
c2031705 <LansdowneOP@cardiff.ac.uk>
2021-12-02 13:30:50 +0000

    All working

--- src/test/java/uk/ac/cardiff/team5/graphium/service/ReadFileTests.java
+++ src/test/java/uk/ac/cardiff/team5/graphium/service/ReadFileTests.java
@@ -1,8 +1,55 @@
-package uk.ac.cardiff.team5.graphium;
+package uk.ac.cardiff.team5.graphium.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import uk.ac.cardiff.team5.graphium.api.controllers.FileSearchAPIController;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Sql(scripts={"/schema-test.sql", "/data-test.sql"})
+@ActiveProfiles("MariaDB")
+@DirtiesContext
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ReadFileTests {
-    
+    @Autowired
+    private FileSearchAPIController fileSearchAPIController;
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void shouldGet2PublicFileAsJSON() throws Exception{
+        System.out.println(fileSearchAPIController.findPublicFiles());
+        mvc.perform(get("/api/publicFiles").contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+
+//    @Test
+//    @ParameterizedTest
+//    @CsvSource({"heart,1","cancer, 3", "dogs, 0", "cats, 2"})
+//    public void shouldGetNCharitiesFromSearchAsJSON(String search, String countAsString) throws Exception{
+//        Integer count = Integer.valueOf(countAsString);
+//        mvc.perform(get("/api/charity?search="+search).contentType(MediaType.APPLICATION_JSON))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$", hasSize(count)));
+//        }
+//    }
+
 }
--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -1,13 +1,6 @@
-drop database if exists `graphium`;
-create database if not exists `graphium`;
-use `graphium`;
-
-drop table if exists `organisation`;
-drop table if exists `role`;
-drop table if exists `users`;
-drop table if exists `files`;
-drop table if exists `partnerships`;
-
+drop database if exists `test`;
+create database if not exists `test`;
+use `test`;
 create table if not exists `organisation` (
                                               `organisation_id` integer auto_increment primary key,
                                               `organisation_name` varchar(127)
@@ -49,68 +42,3 @@ create table if not exists `partnerships`(
                                              `viewing_organisation_id` integer references `organisation`(`organisation_id`)
 );
 
-DELIMITER //
-CREATE PROCEDURE getPossiblePartnerships(IN organisationID varchar(30))
-BEGIN
-SELECT organisation_id, organisation_name FROM organisation WHERE organisation_id NOT IN (SELECT viewing_organisation_id
-                                                                                          FROM organisation
-                                                                                                   JOIN partnerships on partnerships.sharing_organisation_id = organisation.organisation_id
-                                                                                          WHERE organisation.organisation_id = organisationID) and organisation.organisation_id != organisationID;
-
-END //
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE createPartnership(IN organisationID int, IN usernameParameter varchar(30))
-BEGIN
-set @SharingOrganisationID = (Select organisation_id from users where username = usernameParameter);
-INSERT INTO partnerships(sharing_organisation_id,viewing_organisation_id)  values(@SharingOrganisationID,organisationID);
-END //
-DELIMITER ;
-
-
-insert into users (organisation_id, role_id, first_name, last_name, username, email, password, active, organisation_approved, email_verified) values (1, 1, 'User', 'Example', 'user', 'user@example.com', '$2a$10$jy7T4nLKfalHAp/3Dv2qae0Le9.xsSUWnWq9WA2XXiyX2orgIEh9C', true, true, true);
-
-insert into users (organisation_id, role_id, first_name, last_name, username, email, password, active, organisation_approved, email_verified) values (2, 2, 'OrgAdmin', 'Example', 'orgadmin', 'orgadmin@example.com', '$2a$10$/vOrWfM8MJ.Dzpj3t5oGyeuNoERADR5LlEGrV6pwSr0Did8JikTTq', true, true, true);
-
-insert into users (role_id, first_name, last_name, username, email, password, active, organisation_approved, email_verified) values (3, 'SystemAdmin', 'Example', 'sysadmin', 'sysadmin@example.com', '$2a$10$/vOrWfM8MJ.Dzpj3t5oGyeuNoERADR5LlEGrV6pwSr0Did8JikTTq', true, true, true);
-
-DELIMITER //
-CREATE PROCEDURE getFilesForOrganisation(IN usernameParameter varchar(30))
-BEGIN
-set @OrganisationID = (Select organisation_id from users where username = usernameParameter);
-SELECT files.file_id, files.file_name, files.file_type,files.tag,files.access_level, files.comment,files.data, files.date,users.username FROM graphium.files JOIN users on files.user_id = users.user_id JOIN organisation on organisation.organisation_id = users.organisation_id WHERE users.organisation_id = @OrganisationID and files.access_level != 'private' ORDER BY files.date;
-END //
-DELIMITER ;
-
-DELIMITER //
-CREATE PROCEDURE getPartnersFiles(IN usernameParameter varchar(30))
-BEGIN
-set @OrganisationID = (Select organisation_id from users where username = usernameParameter);
-SELECT files.file_id, files.file_name, files.file_type,files.tag,files.access_level, files.comment,files.data, files.date,users.username
-FROM files
-         JOIN users on files.user_id = users.user_id
-         JOIN organisation on organisation.organisation_id = users.organisation_id
-WHERE organisation.organisation_id IN (SELECT sharing_organisation_id
-                                       FROM organisation
-                                                JOIN partnerships on partnerships.sharing_organisation_id = organisation.organisation_id
-                                       WHERE viewing_organisation_id = @OrganisationID) and files.access_level !='private'
-ORDER BY files.date;
-END //
-DELIMITER ;
-DELIMITER //
-CREATE PROCEDURE getAllFiles(IN usernameParameter varchar(30))
-BEGIN
-set @OrganisationID = (Select organisation_id from users where username = usernameParameter);
-
-SELECT files.file_id, files.file_name, files.file_type,files.tag,files.access_level, files.comment,files.data, files.date,users.username
-FROM files
-         JOIN users on files.user_id = users.user_id
-         JOIN organisation on organisation.organisation_id = users.organisation_id
-WHERE (organisation.organisation_id IN (((SELECT sharing_organisation_id
-                                          FROM organisation
-                                                   JOIN partnerships on partnerships.sharing_organisation_id = organisation.organisation_id
-                                          WHERE viewing_organisation_id = @OrganisationID) and files.access_level !='private') and organisation.organisation_id != @organisationID) and files.access_level !='private') or (files.access_level = 'public') or (users.username = usernameParameter) or (organisation.organisation_id = @OrganisationID)
-ORDER BY files.date;
-
-END //
