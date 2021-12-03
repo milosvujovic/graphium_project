c8e2e4e965719cbe60f2bb80eb003f98dd0bbc7a
c2031705 <LansdowneOP@cardiff.ac.uk>
2021-12-03 14:55:31 +0000

    Committing changes

--- src/main/java/uk/ac/cardiff/team5/graphium/files/FileServerDBImpl.java
+++ src/main/java/uk/ac/cardiff/team5/graphium/files/FileServerDBImpl.java
@@ -1,20 +1,16 @@
 package uk.ac.cardiff.team5.graphium.files;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.ac.cardiff.team5.graphium.data.jpa.entity.DBFile;
 import uk.ac.cardiff.team5.graphium.data.jpa.entity.UserEntity;
 import uk.ac.cardiff.team5.graphium.data.jpa.repository.UserRepository;
 import uk.ac.cardiff.team5.graphium.service.dto.FileDTO;
 
-import java.io.IOException;
-import java.sql.SQLOutput;
-import java.util.List;
-
 @Service
 public class FileServerDBImpl implements FileServer{
 
     private UserRepository userRepository;
+
     private DBFileStore dbFileStore;
 
     public FileServerDBImpl(DBFileStore aDbFileStore, UserRepository aUserRepository){
@@ -27,9 +23,7 @@ public class FileServerDBImpl implements FileServer{
         String fileId = null;
 //        Converts FileDTO to a DBFile
         DBFile dbFile = new DBFile(null,aFile.getFileName(),aFile.getType() ,aFile.getTag(),aFile.getAccessLevel() ,aFile.getComment(),aFile.getData(), aFile.getDate());
-
         UserEntity user =  userRepository.findByUsername(username);
-
 //        Adds file to the user
         user.addFile(dbFile);
 //        Updates user in the database
