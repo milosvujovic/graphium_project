472a8b0d9bfc734897938d3f6836d43b6d9a1976
Joseph Williams <williamsj155@cardiff.ac.uk>
2021-12-09 22:20:49 +0000

    create route to get csrf token from session

---
+++ src/main/java/uk/ac/cardiff/team5/graphium/api/controllers/csrf/CSRFAPIController.java
@@ -0,0 +1,20 @@
+package uk.ac.cardiff.team5.graphium.api.controllers.csrf;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController()
+@RequestMapping("/api/csrf")
+public class CSRFAPIController {
+    @GetMapping("")
+    public String get(HttpServletRequest request) {
+        CsrfToken token = new HttpSessionCsrfTokenRepository().loadToken(request);
+        return token.getToken();
+    }
+
+}
