1ce104051313cf253077cd2f68f424d225eb19b2
c2031705 <LansdowneOP@cardiff.ac.uk>
2021-12-09 12:13:54 +0000

    Passes all the unit tests

--- build_db.sql
+++ build_db.sql
@@ -133,7 +133,7 @@ SELECT files.file_id, files.file_name, files.file_type,files.tag,files.access_le
 FROM files
 JOIN users on files.user_id = users.user_id
 JOIN organisation on organisation.organisation_id = users.organisation_id
-where files.access_level = 'public' or users.username = @usernameParameter or ((organisation.organisation_id = @OrganisationID and files.access_level !='private') or (organisation.organisation_id in (SELECT sharing_organisation_id
+where files.access_level = 'public' or users.username = usernameParameter or ((organisation.organisation_id = @OrganisationID and files.access_level !='private') or (organisation.organisation_id in (SELECT sharing_organisation_id
 FROM organisation
 JOIN partnerships on partnerships.sharing_organisation_id = organisation.organisation_id
 WHERE viewing_organisation_id = @OrganisationID) and files.access_level = 'myPartners'));
--- src/test/resources/storedprocedure-test.sql
+++ src/test/resources/storedprocedure-test.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE getFilesForOrganisation(IN usernameParameter varchar(30))
 BEGIN
 set @OrganisationID = (Select organisation_id from users where username = usernameParameter);
 SELECT files.file_id, files.file_name, files.file_type,files.tag,files.access_level, files.comment, files.date,users.username,files.subject,organisation.organisation_name
-FROM graphium.files JOIN users on files.user_id = users.user_id JOIN organisation on organisation.organisation_id = users.organisation_id
+FROM files JOIN users on files.user_id = users.user_id JOIN organisation on organisation.organisation_id = users.organisation_id
 where users.username = usernameParameter or (organisation.organisation_id = @OrganisationID and files.access_level != 'private');
 END $$
 
@@ -29,7 +29,7 @@ SELECT files.file_id, files.file_name, files.file_type,files.tag,files.access_le
 FROM files
          JOIN users on files.user_id = users.user_id
          JOIN organisation on organisation.organisation_id = users.organisation_id
-where files.access_level = 'public' or users.username = @usernameParameter or ((organisation.organisation_id = @OrganisationID and files.access_level !='private') or (organisation.organisation_id in (SELECT sharing_organisation_id
+where files.access_level = 'public' or users.username = usernameParameter or ((organisation.organisation_id = @OrganisationID and files.access_level !='private') or (organisation.organisation_id in (SELECT sharing_organisation_id
                                                                                                                                                                                                         FROM organisation
                                                                                                                                                                                                                  JOIN partnerships on partnerships.sharing_organisation_id = organisation.organisation_id
                                                                                                                                                                                                         WHERE viewing_organisation_id = @OrganisationID) and files.access_level = 'myPartners'));
@@ -38,5 +38,5 @@ END $$
 
 CREATE PROCEDURE getPublicFiles()
 BEGIN
-SELECT files.file_id, files.file_name, files.file_type,files.tag,files.access_level, files.comment, files.date,users.username,files.subject,organisation.organisation_name FROM graphium.files JOIN users on files.user_id = users.user_id  JOIN organisation on organisation.organisation_id = users.organisation_id where files.access_level = 'public' ORDER BY files.date;
+SELECT files.file_id, files.file_name, files.file_type,files.tag,files.access_level, files.comment, files.date,users.username,files.subject,organisation.organisation_name FROM files JOIN users on files.user_id = users.user_id  JOIN organisation on organisation.organisation_id = users.organisation_id where files.access_level = 'public' ORDER BY files.date;
 END $$
