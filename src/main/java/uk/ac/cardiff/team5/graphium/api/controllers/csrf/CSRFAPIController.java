16cad1fc6cf433e78ae501c75daf08b0d0af1010
Joseph Williams <williamsj155@cardiff.ac.uk>
2021-12-13 07:35:17 +0000

    org, user

--- src/main/java/uk/ac/cardiff/team5/graphium/api/controllers/csrf/CSRFAPIController.java
+++ src/main/java/uk/ac/cardiff/team5/graphium/api/controllers/csrf/CSRFAPIController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/csrf")
 public class CSRFAPIController {
     @GetMapping("")
-    public String get(HttpServletRequest request) {
-        CsrfToken token = new HttpSessionCsrfTokenRepository().loadToken(request);
-        return token.getToken();
+    public Map<String, String> get(HttpServletRequest request) {
+        String token = new HttpSessionCsrfTokenRepository().loadToken(request).getToken();
+        return Collections.singletonMap("token", token);
     }
 
 }
